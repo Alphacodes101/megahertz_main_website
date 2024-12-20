247a02a6a2208351f23780cefe177b7ef99b3c4e
rehyan_yadav <ritulyadav1984@gmail.com>
2024-12-21 01:18:47 +0530

    update

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"83bacfc52569459a4a654727cad2546820cb0d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "578995108"
+    serviceWorkerVersion: "3117253193"
   }
 });
